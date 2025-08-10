cf5a4b589fb04c6b3ce9d818fec364044f168572
Abay-Wizard <abaytessema251@gmail.com>
2025-08-10 16:00:20 +0300

    name changed from routers to routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
-import authRouter from './routers/authRouter.js'
+import authRoute from './routes/authRoute.js'
 dotenv.config()
 const app = express()
 
 app.use(express.json())
 app.use(cors())
-app.use('/api/auth', authRouter)
+app.use('/api/auth', authRoute)
 
 
 
